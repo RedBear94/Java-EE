f69cce7d96e752347ce5cf56ddf483f4c35efe7c
Pavel <redbear95@gmail.com>
2021-04-08 03:47:03 +0300

    HomeWork5

--- first-web-app/first-jsf-app/src/main/java/org/example/persist/Product.java
+++ first-web-app/first-jsf-app/src/main/java/org/example/persist/Product.java
@@ -1,15 +1,30 @@
 package org.example.persist;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@NamedQueries({
+        // в query JPQL/HQL - запросы
+        @NamedQuery(name = "deleteProductById", query = "delete from Product p where p.id = :id"),
+        @NamedQuery(name = "findAllProduct", query = "from Product p"),
+        @NamedQuery(name = "count", query = "select count(p) from Product p")
+})
 
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
     private String name;
 
+    @Column
     private String description;
 
+    @Column
     private BigDecimal price;
 
     public Product() {
